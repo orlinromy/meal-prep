a161627e174d483882f1e41fd21f3f05c0fde638
orlinromy <orlinromy@gmail.com>
2023-03-09 11:18:52 +0800

    fix: navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-// import styles from "../styles/NavBar.module.css";
 import icon from "../assets/food-tray.png";
 
 const NavBar = () => {
@@ -8,8 +7,7 @@ const NavBar = () => {
 
   return (
     <div className="border-b-2 pt-0 bg-gradient-to-t from-[#fdf8f3] via-white">
-      <nav className="flex justify-between mt-[10px] ml-[90px] mr-[70%] mb-[10px] ">
-        {/* <span className="flex justify-between w-4/12 h-2/12 mt-[10px] ml-[100px]"> */}
+      <nav className="flex items-center ml-7 my-3 gap-9">
         <img
           style={{ width: "50px", height: "50px" }}
           onClick={() => {
@@ -17,14 +15,9 @@ const NavBar = () => {
           }}
           src={icon}
           className="cursor-pointer"
-        ></img>
-        <p className="m-auto">
-          <NavLink to="/">Home</NavLink>
-        </p>
-        <p className="m-auto">
-          <NavLink to="/create">Create</NavLink>
-        </p>
-        {/* </span> */}
+        />
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/create">Create</NavLink>
       </nav>
     </div>
   );
