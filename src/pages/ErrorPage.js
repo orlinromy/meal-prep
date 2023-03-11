a910999a0cd00abe5062c787095a8baffe73eb46
orlinromy <orlinromy@gmail.com>
2023-03-11 22:23:20 +0800

    fix: error page styling and navigation

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div className="flex flex-column justify-center text-center my-64">
+    <div className="flex flex-column justify-center items-center text-center h-[80vh] overflow-hidden">
       <p className="text-7xl mb-4">🤯</p>
       <p className="text-3xl">
         Sorry, we can't find anything... try again maybe?
       </p>
-      <a href="/">
+      <NavLink to="/create">
         <button className="w-60 bg-[#659B91] text-white p-2.5 rounded-xl text-xl hover:bg-[#517c74] mt-8">
           Take me back
         </button>
-      </a>
+      </NavLink>
     </div>
   );
 };
