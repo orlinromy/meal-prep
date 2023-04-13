e37141f6a5dfead8d9d6469e32ca3a4795c40cb2
orlinromy <orlinromy@gmail.com>
2023-04-14 00:57:11 +0800

    feat: implemented code splitting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Create from "./pages/Create";
 import NavBar from "./components/NavBar";
-import MealPlanner from "./pages/MealPlanner";
-import ErrorPage from "./pages/ErrorPage";
+
+const Home = lazy(() => import("./pages/Home"));
+const Create = lazy(() => import("./pages/Create"));
+const MealPlanner = lazy(() => import("./pages/MealPlanner"));
+const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 function App() {
   const [data, setData] = useState();
@@ -12,12 +13,14 @@ function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create setData={setData} />} />
-        <Route path="/planner" element={<MealPlanner data={data} />} />
-        <Route path="/error" element={<ErrorPage />} />
-      </Routes>
+      <Suspense fallback={<div />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create setData={setData} />} />
+          <Route path="/planner" element={<MealPlanner data={data} />} />
+          <Route path="/error" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
