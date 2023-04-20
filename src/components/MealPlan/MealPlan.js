e9a84a7bd40afb1572622236cd8f4aa4f8873cab
orlinromy <orlinromy@gmail.com>
2023-04-21 00:43:07 +0800

    fix: change zindex to prevent the card from showing on top of the recipe on mobile view

--- src/components/MealPlan/MealPlan.js
+++ src/components/MealPlan/MealPlan.js
@@ -128,9 +128,7 @@ const MealPlan = (props) => {
                 <table>
                   <tbody>{mealLabel().map((el) => el)}</tbody>
                 </table>
-                <table
-                  style={{ position: "relative !important", zIndex: 9999 }}
-                >
+                <table style={{ position: "relative !important", zIndex: 999 }}>
                   <DragDropContext onDragEnd={handleDragEnd}>
                     <Droppable droppableId="mealCards">
                       {(provided) => (
