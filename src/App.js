424d72f69abd18adde5e2f854111116d57e773c6
orlinromy <orlinromy@gmail.com>
2023-04-29 13:36:47 +0800

    fix: use error boundary to show error page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorPage from "./pages/ErrorPage";
 
 const Home = lazy(() => import("./pages/Home"));
 const Create = lazy(() => import("./pages/Create"));
 const MealPlanner = lazy(() => import("./pages/MealPlanner"));
-const ErrorPage = lazy(() => import("./pages/ErrorPage"));
 
 function App() {
   const [data, setData] = useState();
@@ -13,14 +14,16 @@ function App() {
   return (
     <>
       <NavBar />
-      <Suspense fallback={<div />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<Create setData={setData} />} />
-          <Route path="/planner" element={<MealPlanner data={data} />} />
-          <Route path="/error" element={<ErrorPage />} />
-        </Routes>
-      </Suspense>
+      <ErrorBoundary FallbackComponent={ErrorPage}>
+        <Suspense fallback={<div />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create setData={setData} />} />
+            <Route path="/planner" element={<MealPlanner data={data} />} />
+            <Route path="/error" element={<ErrorPage />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
 const ErrorPage = () => {
   return (
     <div className="flex flex-column justify-center items-center text-center h-[80vh] overflow-hidden px-3">
       <p className="text-7xl mb-4">🤯</p>
-      <p className="text-3xl">
-        Sorry, we can't find anything... try again maybe?
-      </p>
-      <NavLink to="/create">
-        <button className="w-60 bg-[#659B91] text-white p-2.5 rounded-xl text-xl hover:bg-[#517c74] mt-8">
-          Take me back
-        </button>
-      </NavLink>
+      <p className="text-3xl">Oops, something went wrong</p>
+      <p className="text-xl mt-4">...try again maybe?</p>
+      <button
+        onClick={() => {
+          window.location.reload();
+        }}
+        className="w-60 bg-[#659B91] text-white p-2.5 rounded-xl text-xl hover:bg-[#517c74] mt-8"
+      >
+        Take me back
+      </button>
     </div>
   );
 };
