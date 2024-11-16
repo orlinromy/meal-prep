8b32c14fe1f0bcafd9ae6d508b53aa46d77c50d8
orlinromy <orlinromy@gmail.com>
2024-11-16 14:34:34 +0800

    fix: planner page styling

--- src/components/MealPlan/MealCard.js
+++ src/components/MealPlan/MealCard.js
@@ -1,67 +1,68 @@
-import { Card, Box, Chip } from "@mui/material";
-import React from "react";
+import { Card, Box, Chip, Skeleton } from '@mui/material'
+import React, { useState } from 'react'
 
 const MealCard = (props) => {
   function handleClick(e) {
-    props.doubleClicked(props.data);
+    props.doubleClicked(props.data)
   }
 
   function mealLabel(i) {
     switch (i % props.meal) {
       case 0:
-        return "Breakfast";
+        return 'Breakfast'
       case 1:
-        return "Lunch";
+        return 'Lunch'
       case 2:
-        return "Dinner";
+        return 'Dinner'
       case 3:
-        return "Snack";
+        return 'Snack'
       case 4:
-        return "Teatime";
+        return 'Teatime'
     }
   }
 
   return (
     <Box
+      display="flex"
       sx={{
-        height: "104px",
+        height: '104px',
         boxShadow:
-          "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
-        borderRadius: "0.5rem",
-        border: "1px",
-        borderColor: "rgba(255, 255, 255, 0.12)",
-        backgroundColor: "white",
+          '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+        borderRadius: '0.5rem',
+        border: '1px',
+        borderColor: 'rgba(255, 255, 255, 0.12)',
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        padding: 1,
+        gap: 1
       }}
       onClick={handleClick}
     >
-      <Box
-        display="flex"
-        flexDirection="row"
-        alignItems="center"
-        sx={{ height: "100%", mx: 2 }}
-      >
-        <img
-          style={{ width: "80px", height: "80px", marginRight: "8px" }}
+      <Box className="aspect-square h-auto">
+        <Box
+          className="rounded-lg w-full h-full"
+          component="img"
           src={props.data.recipe.images.THUMBNAIL.url}
           alt={props.data.recipe.label}
-          className="rounded-lg"
-          loading="lazy"
         />
-        <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-between"
-        >
-          <p className="text-sm mb-2 ml-2 mt-2">{props.data.recipe.label}</p>
-          <Chip
-            label={mealLabel(props.idx)}
-            size="small"
-            className="w-fit ml-1"
-          />
-        </Box>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        gap={1}
+      >
+        <p className="text-sm ml-2 mr-1 text-ellipsis overflow-hidden">
+          {props.data.recipe.label}
+        </p>
+        <Chip
+          label={mealLabel(props.idx)}
+          size="small"
+          className="w-fit ml-1"
+        />
       </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default MealCard;
+export default MealCard
--- src/components/Recipe/RecipeContainer.js
+++ src/components/Recipe/RecipeContainer.js
@@ -1,29 +1,27 @@
-import React from "react";
-import NutritionTable from "./NutritionTable";
-import styles from "../../styles/RecipeContainer.module.css";
+import React from 'react'
+import NutritionTable from './NutritionTable'
+import styles from '../../styles/RecipeContainer.module.css'
 
 const RecipeContainer = (props) => {
-  const { recipeToShow } = props;
+  const { recipeToShow, source } = props
 
+  const width = source ?? '' === 'drawer' ? '' : 'w-3/5'
   return recipeToShow ? (
     <div
-      className={`pt-8 px-4 text-center ${styles.recipeCont} grow-[4] max-h-[91.7vh] overflow-y-auto `}
+      className={`pt-8 px-4 text-center ${styles.recipeCont} max-h-[91.7vh] overflow-y-auto ${width}`}
     >
       <img
-        className="rounded-md mx-auto"
+        className="rounded-md mx-auto object-cover aspect-square w-11/12"
         alt={recipeToShow.recipe.label}
         src={recipeToShow.recipe.images.REGULAR.url}
         loading="lazy"
-        style={{
-          width: `${recipeToShow.recipe.images.REGULAR.width}`,
-          height: `${recipeToShow.recipe.images.REGULAR.height}`,
-        }}
-      ></img>
+      />
       <h2 className="text-4xl mt-4 mb-2">{recipeToShow.recipe.label}</h2>
       <p className="text-lg">
-        {recipeToShow.recipe.yield}{" "}
-        {recipeToShow.recipe.yield > 1 ? "servings" : "serving"} |{" "}
-        {Math.floor(recipeToShow.recipe.calories)} calories
+        {`${recipeToShow.recipe.yield} ${
+          'serving' + (recipeToShow.recipe.yield > 1 ? 's' : '')
+        } | ${Math.floor(recipeToShow.recipe.calories)} calories`}
+        s
       </p>
       <br />
       <a href={recipeToShow.recipe.url} target="_blank" rel="noreferrer">
@@ -41,16 +39,18 @@ const RecipeContainer = (props) => {
 
       <p className="text-lg text-left mt-4">Health Labels: </p>
       <p className="text-md text-left mb-4">
-        {recipeToShow.recipe.healthLabels.join(", ")}
+        {recipeToShow.recipe.healthLabels.join(', ')}
       </p>
       <p className="text-lg text-left mt-4 mb-2">Nutrition</p>
       <NutritionTable nutrients={recipeToShow.recipe.digest} />
     </div>
   ) : (
-    <div className="text-slate-400 text-2xl text-center align-middle my-auto grow-[4]">
+    <div
+      className={`text-slate-400 text-2xl text-center align-middle my-auto ${width}`}
+    >
       <p>Click on any meal to get the recipe</p>
     </div>
-  );
-};
+  )
+}
 
-export default RecipeContainer;
+export default RecipeContainer
